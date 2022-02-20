9002290db68c9547e67db6edfd247589a6388076
pratiks29 <67452617+pratiks29@users.noreply.github.com>
2022-02-20 11:36:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import faker from '@faker-js/faker' 
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "@faker-js/faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-const App  = function(){
-    return (
-        <div className ="ui container comments">
-            <div className='comment'>
-                <a href='/' className='avator'>
-                    <img src='faker.image.image()' alt='avator'/>
-                </a>
-                <div className='content'>
-                    <a href ='/' className='author'>Sam</a>
-                    <div className='metadata'>
-                        <span className='date'>today at6:00 pm</span>                       
-                    </div>
-                    <div className='text'>nice cbro</div>
-                </div>
-            </div>
-        </div>
-    );
+const App = function () {
+  return (
+    <div className="ui container comments">
+        <ApprovalCard>
+      <CommentDetail
+        author="mr bruh"
+        timeAgo="today at 4:69am"
+        comment="this was good blogpost"
+        imagep="faker.image.image()"
+      />
+      </ApprovalCard>
+      <ApprovalCard>
+      <CommentDetail
+        author="samuel"
+        timeAgo="yesterday at 7:09pm "
+        comment="wow it was a great read! thank you so much author"
+        imagep="faker.image.image()"
+      />
+      </ApprovalCard>
+      <ApprovalCard>warning! do you really want to do this </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
